1760471f448f4a458e973fe92a55665749fced9f
kisks5 <act_bm1239@yahoo.co.jp>
2012-07-29 10:59:26 +0900

    Update wp-content/themes/hccc/header.php

--- wp-content/themes/hccc/header.php
+++ wp-content/themes/hccc/header.php
@@ -28,6 +28,20 @@
   })();
 
 </script>
+<script type="text/javascript">
+	$(function(){
+	    var setImg = '#viewer';
+	    var fadeSpeed = 1500;
+	    var switchDelay = 5000;
+	 
+	$(setImg).children('img').css({opacity:'0'});
+	$(setImg + ' img:first').stop().animate({opacity:'1',zIndex:'20'},fadeSpeed);
+	 
+	setInterval(function(){
+		$(setImg + ' :first-child').animate({opacity:'0'},fadeSpeed).next('img').animate({opacity:'1'},fadeSpeed).end().appendTo(setImg);
+	    },switchDelay);
+	});
+</script>
 </head>
 
 <body>
